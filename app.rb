a6ed56faf3a2b3a7234240a2cec58dcedf63e78e
Bonface Masira <bonfacemasira@gmail.com>
2022-08-14 01:35:56 +0300

    Done

---
+++ app.rb
@@ -0,0 +1,11 @@
+puts ("Hello World!")
+# puts ("Hello Bonface Nyachuba Masira!")
+# puts ("Hello Bonface Nyachuba Masira!")
+print "Pass this test, please."
+# print ("Hello Bonface Nyachuba Masira!")
+# print ("Hello Bonface Nyachuba Masira!")
+# puts [1,2,3,4,5,6]
+# p [1,2,3,4,5,6]
+p [1, 2, 3]
+# pp [{id: 1, hello: "World"}, {id: 2, hello: "Ruby"}, {id: 3, hello: "Moon"}, {id: 4, hello: "Learner"}]
+pp [{id: 1, hello: "World"}, {id: 2, hello: "Ruby"}, {id: 3, hello: "Moon"}, {id: 4, hello: "Learner"}]
